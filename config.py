e50a2f4d4c2d63a438fc07f42b4aa405fd19d7db
ShawnChew <shawnchew200@gmail.com>
2025-04-11 04:47:39 +0800

    Update config.py to standardise hyperparameter for results

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 CONFIG = {
     "model": "cnn_base",                   # Model type: ['cnn_base', 'cnn_pretrained', 'rnn_base']
     "batch_size": 64,
-    "epochs": 5,
+    "epochs": 15,
     "learning_rate": 0.001,
-    "val_split": 0,
+    "val_split": 0.2,
     "use_colour": False, # set to true for RGB input
     "image_size": (28, 28),                   # Can be set to None if no resizing is wanted
     "train_path": "dataset/train_letter",
@@ -12,10 +12,10 @@ CONFIG = {
 
 # CONFIG = {
 #     "model": "rnn_base",                  
-#     "batch_size": 16,
-#     "epochs": 10,
+#     "batch_size": 64,
+#     "epochs": 15,
 #     "learning_rate": 0.001,
-#     "val_split": 0,
+#     "val_split": 0.2,
 #     "use_colour": False, 
 #     "image_size": (200, 50),  
 #     "train_path": "dataset/train",
